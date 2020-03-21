f2459c51a352dca5d4b00816ad50ce4c72667650
Draxcel <50392714+draxcel@users.noreply.github.com>
2020-03-22 00:55:36 +0900

    Add files via upload

--- financial-review-scraper.py
+++ financial-review-scraper.py
@@ -1,20 +1,27 @@
-# Financial Review Equity Market Scraper.
-# Developer: Jun-hyeong Kim.
-# Any inquiry: draxcel.blackfins@gmail.com
-
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from datetime import datetime
+#from pandas import DataFrame
 
-now = datetime.now()
-print('Date: {}/{}/{}'.format(now.year, now.month, now.day))
-print('Time: {}:{}'.format(now.hour, now.minute), '\n')
+import datetime
+import pandas_datareader.data as web
+
+now = datetime.datetime.now()
+nowDate = now.strftime('%Y-%m-%d')
+nowTime = now.strftime('%H:%M:%S')
+yesDate = now - datetime.timedelta(1)
+
+print('Date: ', nowDate)
+print('Time: ', nowTime, '\n')
+
+asx200 = web.DataReader('^AXJO', 'yahoo', yesDate, nowDate)
+print("Today's S&P/ASX 200:", round(asx200.iloc[0, 2]), '\n')
+print('<Financial Review Today>', '\n')
 
 html = urlopen('https://www.afr.com/markets/equity-markets')
 soup = BeautifulSoup(html.read(), 'html.parser')
 
 titleList = soup.findAll('a', {'class':'_20-Rx'})
 for title in titleList:
-    print(title.get_text(), '\n')
+    print('●', title.get_text(), '\n')
 
-print('☘ Good luck with your investment.')
+print('-https://www.afr.com/markets/equity-markets-')
