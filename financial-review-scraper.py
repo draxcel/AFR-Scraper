8e30780238fe2acd5cb4f348095955ffe7ccd1f9
Draxcel <50392714+draxcel@users.noreply.github.com>
2020-03-25 13:42:58 +0900

    Add files via upload

--- financial-review-scraper.py
+++ financial-review-scraper.py
@@ -8,7 +8,7 @@ now = datetime.datetime.now()
 nowDate = now.strftime('%Y-%m-%d')
 nowTime = now.strftime('%H:%M:%S')
 
-startDate = now + datetime.timedelta(days = -7)
+startDate = now - datetime.timedelta(days = 7)
 
 print('Date: ', nowDate)
 print('Time: ', nowTime, '\n')
@@ -17,10 +17,10 @@ asx200 = web.DataReader('^AXJO', 'yahoo', startDate, nowDate)
 asx200 = asx200.reset_index()
 asx200 = asx200.loc[::-1]
 
-asx200_today = round(asx200.iloc[0, 6])
-asx200_yesterday = round(asx200.iloc[1, 6])
+asx200_today = round(asx200.iloc[0, 6], 2)
+asx200_yesterday = round(asx200.iloc[1, 6], 2)
 
-asx200_diff = asx200_today - asx200_yesterday
+asx200_diff = round(asx200_today - asx200_yesterday, 2)
 asx200_diffper = abs((asx200_diff / asx200_yesterday) * 100)
 
 if asx200_today > asx200_yesterday:
@@ -31,7 +31,7 @@ else:
     sign = '▼'
 
 print("Current S&P/ASX 200 Close:", asx200_today,
-      sign, asx200_diff, '(', sign, round(asx200_diffper), '%', ')','\n')
+      sign, asx200_diff, '(', sign, round(asx200_diffper, 2), '%', ')','\n')
 print('<Financial Review Today>', '\n')
 
 html = urlopen('https://www.afr.com/markets/equity-markets')
