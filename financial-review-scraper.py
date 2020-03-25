6f576f63d6e0e3bee3093701f9afdc234340f5bb
Draxcel <50392714+draxcel@users.noreply.github.com>
2020-03-25 13:34:17 +0900

    Add files via upload

--- financial-review-scraper.py
+++ financial-review-scraper.py
@@ -1,19 +1,37 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
 import datetime
 import pandas_datareader.data as web
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 now = datetime.datetime.now()
 nowDate = now.strftime('%Y-%m-%d')
 nowTime = now.strftime('%H:%M:%S')
-yesDate = now - datetime.timedelta(1)
+
+startDate = now + datetime.timedelta(days = -7)
 
 print('Date: ', nowDate)
 print('Time: ', nowTime, '\n')
 
-asx200 = web.DataReader('^AXJO', 'yahoo', yesDate, nowDate)
-print("Current S&P/ASX 200 Close:", round(asx200.iloc[0, 5]), '\n')
+asx200 = web.DataReader('^AXJO', 'yahoo', startDate, nowDate)
+asx200 = asx200.reset_index()
+asx200 = asx200.loc[::-1]
+
+asx200_today = round(asx200.iloc[0, 6])
+asx200_yesterday = round(asx200.iloc[1, 6])
+
+asx200_diff = asx200_today - asx200_yesterday
+asx200_diffper = abs((asx200_diff / asx200_yesterday) * 100)
+
+if asx200_today > asx200_yesterday:
+    sign = '▲'
+elif asx200_today == asx200_yesterday:
+    sign = ''
+else:
+    sign = '▼'
+
+print("Current S&P/ASX 200 Close:", asx200_today,
+      sign, asx200_diff, '(', sign, round(asx200_diffper), '%', ')','\n')
 print('<Financial Review Today>', '\n')
 
 html = urlopen('https://www.afr.com/markets/equity-markets')
